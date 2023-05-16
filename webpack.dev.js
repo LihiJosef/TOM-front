2244bb5dcb60c8bb2715f6a3068f4d4b0488a944
Lihi Yosef <322816679@idf.il>
2023-05-16 11:38:26 +0300

    stats

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
     publicPath: "/"
   },
   devServer: {
-    stats: "none",
+    // stats: "none",
     static: { directory: path.join(__dirname, "public") },
     port: 3000,
     hot: true,
